cfdbff3496d5ee5d782487aef53d81b7507d9bfc
Christopher Grant <cgrant@andrew.cmu.edu>
2013-04-23 20:21:13 -0400

    correct article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ActiveRecord::Base
+    attr_accessible :title, :content, :category_id, :active
+    belongs_to :category 
+    
+    validates_presence_of :title
+    validates_presence_of :content
+    
+    scope :alphabetical, order('title DESC')
+    scope :active, where('active = ?', true)
+    
 end
